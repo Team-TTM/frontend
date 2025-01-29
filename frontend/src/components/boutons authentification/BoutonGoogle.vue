678c189e63cff9f9b32c4791c3bcd699bc6b49e7
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-01-29 16:09:43 +0100

    edit : google integration front ca marche un peu mieux

--- frontend/src/components/boutons authentification/BoutonGoogle.vue
+++ frontend/src/components/boutons authentification/BoutonGoogle.vue
@@ -1,44 +1,24 @@
 <template>
-  <div>
-    <!-- Configuration du bouton Google -->
-    <div
-      id="g_id_onload"
-      :data-client_id="clientId"
-      data-context="signin"
-      data-ux_mode="popup"
-      :data-login_uri="loginUri"
-      data-auto_prompt="false">
-    </div>
-
-    <!-- Bouton Google -->
-    <div class="g_id_signin"
-         data-type="standard"
-         data-shape="rectangular"
-         data-theme="filled_blue"
-         data-text="signin_with"
-         data-size="large"
-         data-logo_alignment="left">
-    </div>
-  </div>
+  <button @click="redirectToGoogleOAuth" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+    Se connecter avec Google
+  </button>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      clientId: "758592485171-8s2snaqkej804kfe3pt2sifnuvfs5j9o.apps.googleusercontent.com",
-      loginUri: "http://localhost:3000/users/auth/google"
-    };
-  },
-  mounted() {
-    // Charger dynamiquement le script Google si ce n'est pas encore fait
-    if (!window.google) {
-      const script = document.createElement("script");
-      script.src = "https://accounts.google.com/gsi/client";
-      script.async = true;
-      script.defer = true;
-      document.head.appendChild(script);
-    }
-  }
+<script setup>
+const GOOGLE_OAUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+const GOOGLE_CLIENT_ID = "758592485171-8s2snaqkej804kfe3pt2sifnuvfs5j9o.apps.googleusercontent.com"
+const GOOGLE_CALLBACK_URL = "http://localhost:3000/users/google/callback"
+const GOOGLE_OAUTH_SCOPES = [
+  "email",
+  "profile",
+  "openid"
+];
+
+const redirectToGoogleOAuth = () => {
+  const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
+  const state = "some_state";
+  const url = `${GOOGLE_OAUTH_URL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}&access_type=offline&response_type=code&state=${state}&scope=${encodeURIComponent(scopes)}`;
+
+  window.location.href = url;
 };
 </script>
