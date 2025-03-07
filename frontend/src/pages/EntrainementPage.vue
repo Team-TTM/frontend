4e622580ab2ea8e0d1f68c2930337e38b3f0ee88
Gabriel GREZIT <gabriel.grezit@gmail.com>
2025-03-07 23:46:37 +0100

    add : ajout page Stage, page Entrainement, page Club et page Evènement et les routes associées

---
+++ frontend/src/pages/EntrainementPage.vue
@@ -0,0 +1,32 @@
+<script setup>
+import LogoTTM from "@/components/LogoTTM.vue";
+import BoutonsHeader from "@/components/boutonsHeader.vue";
+</script>
+
+<template>
+  <div id="page-container">
+    <header>
+      <LogoTTM/>
+      <boutons-header/>
+    </header>
+    <div id="EntrainementText">
+      Vous êtes sur la page des entraînements
+    </div>
+    <footer>
+      © 2025 - Site TTM | Auteur | Support
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+#EntrainementText{
+  position: absolute;
+  top:50%;
+  left:40%;
+  color:red;
+  font-weight: bold;
+  font-size: 20px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+</style>
---
+++ frontend/src/pages/StagePage.vue
@@ -0,0 +1,31 @@
+<script setup>
+import LogoTTM from "@/components/LogoTTM.vue";
+import BoutonsHeader from "@/components/boutonsHeader.vue";
+</script>
+
+<template>
+  <div id="page-container">
+    <header>
+      <LogoTTM/>
+      <boutons-header/>
+    </header>
+    <div id="StageText">
+      Vous êtes sur la page de stage
+    </div>
+    <footer>
+      © 2025 - Site TTM | Auteur | Support
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+#StageText{
+  position: absolute;
+  top:50%;
+  left:40%;
+  color:red;
+  font-weight: bold;
+  font-size: 20px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+</style>
