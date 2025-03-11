1967bfda2e120e65e29113087207c3b3098a7788
flaw03 <100543813+flaw03@users.noreply.github.com>
2025-03-11 22:29:55 +0100

    refactor: Reorganisation de la structure

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import '@/styles/globalStyles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 import { store } from "./store/index.js"
 
 const app = createApp(App)
