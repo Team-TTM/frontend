e930802dc6233a144ac2b1ded0cabe43a941d0c4
Gabriel GREZIT <gabriel.grezit@gmail.com>
2025-04-03 12:50:36 +0200

    Fix : visibilité de tous les adhérents

--- src/pages/CsvListeAdherent.vue
+++ src/pages/CsvListeAdherent.vue
@@ -441,7 +441,7 @@ export default {
   background: white;
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  overflow: hidden;
+  overflow: auto;
   padding: 20px;
   display: flex;
   flex-direction: column;
